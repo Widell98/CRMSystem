1fe192ddda7878e4be6a19361f933fd6e035d0ba
David Vallin <dv.code@outlook.com>
2022-04-20 12:18:27 +0200

    Table-for-people

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import './App'
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 import OneNavBar from './Components/nav'
 import React from 'react'
+import TableInformation from './TableInfo.js'
 
 const App = () => {
   return (
-    <OneNavBar />
+    <div>
+      <OneNavBar />
+      <TableInformation />
+    </div>
 
   )
 
