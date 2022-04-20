db68fbb88d45140423484b274c9b769ece44cf7c
David Vallin <dv.code@outlook.com>
2022-04-20 11:15:06 +0200

    NavbarClean

--- src/App.js
+++ src/App.js
@@ -1,37 +1,14 @@
 import './App'
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+import OneNavBar from './Components/nav'
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Navbar collapseOnSelect expand="lg" bg="danger" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Among People</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Nav>
-              <Nav.Link href="#deets">under work</Nav.Link>
-              <Nav.Link eventKey={2} href="#memes">
-                About us!
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+    <OneNavBar />
 
-    </div>
-  );
+  )
+
 }
 
 export default App;
