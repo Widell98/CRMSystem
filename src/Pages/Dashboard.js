de79a4170f79543eb1932a3ccddf1ac2ad4ee2ce
Erik <erik.widell@hotmail.com>
2022-04-21 16:15:02 +0200

    switched page

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,6 +1,13 @@
+import TicketCard from '../components/TicketCard'
+
 const Dashboard = () => {
     return(
-        <div>Dashboard</div>
+        <div className="dashboard">
+            <h1> My projects</h1>
+            <div className="ticket-container">
+                <TicketCard/>
+            </div>
+        </div>
     )
 }
 
