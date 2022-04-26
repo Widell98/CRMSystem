28b1b242eb92c8666661ef341bb51ecd2956d27d
Widell98 <92434835+Widell98@users.noreply.github.com>
2022-04-26 15:08:00 +0200

    map data

---
+++ server.js
@@ -0,0 +1,60 @@
+const PORT = 8000
+const express = require('express')
+const cors = require('cors') 
+require('dotenv').config()
+const axios = require('axios')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const url = 'https://07dd3ed3-c8ec-4544-9917-cd74580d8ed3-us-east1.apps.astra.datastax.com/api/rest/v2/namespaces/clients/collections/tasks'
+const token = 'AstraCS:ROaZebRsNmEshfrRGqZFySqJ:94060cfd1ebc7efb58537b922b4c69d481af62ac8fe2c80de7dafc385131af6f'
+
+
+app.get('/clients', async ( req, res) => {
+    const options = {
+        method: 'GET',
+        headers: {
+            Accepts: 'application/json',
+            'X-Cassandra-Token': token,
+            
+        },
+    }
+    try {
+        const response = await axios(`${url}?page-size=20`, options)
+        res.status(200).json(response.data)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({message: err})
+    }
+})
+
+
+
+app.post('/clients', async (req, res) => {
+    const formData = req.body.formData
+
+    const options = {
+        method: 'POST',
+        headers: {
+            Accepts: 'application/json',
+            'X-Cassandra-Token': token,
+            'Content-Type': 'application/json'
+        },
+        data: formData
+    }
+
+    try {
+        const response = await axios(url, options)
+        res.status(200).json(response.data)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({message: err})
+    }
+
+})
+
+
+
+app.listen(PORT, () => console.log('listening on port' + PORT))
--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useState } from 'react'
 import Dashboard from './Pages/Dashboard'
 import TicketPage from './Pages/TicketPage'
 import EmployeePage from './Pages/EmployeePage'
 import Navbar from './Components/Navbar'
+import CalenderPage from './Pages/Calender'
+import CategoriesContext from './context'
 
 const App = () => {
+  const [categories, setCategories] = useState(null)
+  const value = { categories, setCategories }
 
   return (
     <div className="app">
+      <CategoriesContext.Provider value={value}>
      <BrowserRouter>
      <Navbar/>
      <Routes>
+
        <Route path='/' element={<Dashboard/>}/>
        <Route path='/ticket' element={<TicketPage/>}/>
        <Route path='/EmployeePage' element={<EmployeePage/>}/>
+       <Route path='/CalenderPage' element={<CalenderPage/>}/>
 
      </Routes>
      </BrowserRouter>
+     </CategoriesContext.Provider>
     </div>
   )
 }
